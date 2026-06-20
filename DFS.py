8596eb54c471cd4db8e15b33446169af868836a4
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-06-21 03:18:10 +0600

    DFS

--- DFS.py
+++ DFS.py
@@ -1,3 +1,25 @@
 from collections import deque
-
+start='A'
 stack=deque()
+stack.append(start)
+visited=set()
+goal="G"
+path=""
+graph={}
+while stack:
+    node=stack.pop()
+    if node in visited:
+        continue
+    else:
+        visited.add(node)
+    if node==goal:
+        print(path)
+        break
+    for n ,w in graph[node]:
+        if n not in visited:
+            new_path=path+n
+            stack.append(n)
+
+
+
+    
